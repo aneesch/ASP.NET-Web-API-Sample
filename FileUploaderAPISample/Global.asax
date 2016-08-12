9618f3b1c283c63b8730c0e7cc0e60b1d256b6b5
Anees.C.H <anees.ch@outlook.com>
2016-08-12 11:48:45 -0700

    first commit

---
+++ FileUploaderAPISample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileUploaderAPISample.Global" Language="C#" %>
---
+++ FileUploaderAPISample/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Http;
+
+namespace FileUploaderAPISample
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+        }
+        
+    }
+}
